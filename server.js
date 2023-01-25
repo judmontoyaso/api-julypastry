a8e20270bffa43dfc4c9c9559ee7f71d96627cc7
Juan David Montoya <82336705+judmontoyaso@users.noreply.github.com>
2023-01-25 16:32:09 +0000

    Configurar cors

--- server.js
+++ server.js
@@ -19,7 +19,25 @@ const port = process.env.PORT || 5000;
 const app = Express();
 
 app.use(Express.json());
-app.use(Cors());
+app.use(Cors({origin: 'https://july-pastry-app.vercel.app'}));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://july-pastry-app.vercel.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 var jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
